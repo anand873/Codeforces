9a3fee531e67b04698b8dd9f2d8f233121965aa6
Anand Raj <anandraj365@yahoo.in>
2020-08-12 01:35:00 +0530

    Add solution for problem `A - Cut Ribbon` Link: https://codeforces.com/contest/189/problem/A Tags: brute force, dp, *1300 Ref: https://codeforces.com/contest/189/submission/89606914

---
+++ codeforces/189/A.cpp
@@ -0,0 +1,70 @@
+//Author: AnandRaj doubleux
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pll> vpll; 
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define test() int t;cin>>t;while(t--)
+#define all(v) v.begin(),v.end()
+#define prinp(p) cout<<p.first<<" "<<p.second<<endl
+#define prinv(V) for(auto v:V) cout<<v<<" ";cout<<endl
+#define take(V,f,n) for(int in=f;in<f+n;in++) cin>>V[in]
+#define what(x) cerr<<#x<<" = "<<x<<endl
+#define KStest() int t,t1;cin>>t;t1=t;while(t--)
+#define KScout cout<<"Case #"<<t1-t<<": "
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int MOD = 1e9+7,MAX = 1e5+5;
+const ll INF = 1e18+5;
+
+//////////////////Sieve////////////////////
+vector<bool> is_prime(MAX, true);
+vi MinDiv(MAX);
+void Sieve()
+{
+	is_prime[0] = is_prime[1] = false;
+	int i,j;
+	for (i = 2; i*i <= MAX; i++) 
+	{
+    	if (is_prime[i]) 
+    	{
+    		MinDiv[i]=i;
+        	for (j = i * i; j <= MAX; j += i)
+            {
+				is_prime[j] = false;
+				MinDiv[j]=i;
+    		}
+    	}
+	}
+	for(int i=2;i<=MAX;i++) if(is_prime[i]) MinDiv[i]=i;
+}
+//////////////////Sieve////////////////////
+
+
+int main()
+{
+	int n,a,b,c;
+	cin>>n>>a>>b>>c;
+	int ans=0;
+	for(int x=0;x<=n;x++)
+	{
+		for(int y=0;y<=n;y++)
+		{
+			int sum = a*x+b*y;
+			if(sum<=n&&(n-sum)%c==0)
+			{
+				ans = max(ans,x+y+(n-sum)/c);
+			}
+		}
+	}
+	cout<<ans<<endl;
+}	
