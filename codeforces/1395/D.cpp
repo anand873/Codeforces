8af6e03b2970bd9e0930a4fc5073d6cb4482a0d1
Anand Raj <anandraj365@yahoo.in>
2020-08-12 21:49:00 +0530

    Add solution for problem `D - Boboniu Chats with Du` Link: https://codeforces.com/contest/1395/problem/D Tags: brute force, dp, greedy, sortings, *1800 Ref: https://codeforces.com/contest/1395/submission/89720397

---
+++ codeforces/1395/D.cpp
@@ -0,0 +1,88 @@
+//Author: AnandRaj doubleux
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pll> vpll; 
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define test() int t;cin>>t;while(t--)
+#define all(v) v.begin(),v.end()
+#define prinp(p) cout<<p.first<<" "<<p.second<<endl
+#define prinv(V) for(auto v:V) cout<<v<<" ";cout<<endl
+#define take(V,f,n) for(int in=f;in<f+n;in++) cin>>V[in]
+#define what(x) cerr<<#x<<" = "<<x<<", "
+#define KStest() int t,t1;cin>>t;t1=t;while(t--)
+#define KScout cout<<"Case #"<<t1-t<<": "
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int MOD = 1e9+7,MAX = 1e6+5;
+const ll INF = 1e18+5;
+
+vl A;
+vl G,B;
+
+ll ok(ll mid,ll d)
+{
+	// cerr<<mid<<" : ";
+	ll Bads = B[mid];
+	// what(B[mid]);
+	int waste = max(0ll,(mid-1)*d);
+	// what(waste);
+	waste -= (B.size()-1-mid);
+	waste = max(0,waste);
+	// what(waste);
+	int good = G.size()-1 - waste;
+	// what(good);
+	// what(G[good]);
+	Bads += G[good];
+	// cout<<endl;
+	return Bads;
+}
+
+int main()
+{
+	ll n,d,m;
+	cin>>n>>d>>m;
+
+	A.resize(n);
+	take(A,0,n);
+	sort(all(A));
+	int how=0;
+	for(int i=0;i<n;i++)
+	{
+		if(A[i]<=m) G.push_back(A[i]);
+		else {B.push_back(A[i]);how++;}
+	}
+	G.push_back(0);
+	B.push_back(0);
+	reverse(all(B));
+	reverse(all(G));
+
+	for(int i=1;i<(int)B.size();i++)
+	{
+		B[i]+=B[i-1];
+	}
+	for(int i=1;i<(int)G.size();i++)
+	{
+		G[i]+=G[i-1];
+	}
+
+	ll ans =0;
+	for(int mid = 0;mid<=how;mid++)
+	{
+		if(mid + (mid-1)*d>n) break;
+		ans = max(ans,ok(mid,d));
+	}
+
+	cout<<ans<<endl;
+
+
+}	
