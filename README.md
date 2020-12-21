f3bdae88cdb67077156ddeb1fcf864eb1c010b79
Anand Raj <anandraj365@yahoo.in>
2020-12-21 20:35:00 +0530

    Add solution for problem `B - Fair Numbers` Link: https://codeforces.com/contest/1465/problem/B Tags: brute force, number theory Ref: https://codeforces.com/contest/1465/submission/101972828

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-1 | [C - Peaceful Rooks](https://codeforces.com/contest/1465/problem/C) | [GNU C++14](./codeforces/1465/C.cpp) | `dfs and similar` `dsu` `greedy` `implementation` | Dec/21/2020 20:37 | 
+2 | [C - Peaceful Rooks](https://codeforces.com/contest/1465/problem/C) | [GNU C++14](./codeforces/1465/C.cpp) | `dfs and similar` `dsu` `greedy` `implementation` | Dec/21/2020 20:37 | 
+1 | [B - Fair Numbers](https://codeforces.com/contest/1465/problem/B) | [GNU C++14](./codeforces/1465/B.cpp) | `brute force` `number theory` | Dec/21/2020 20:35 | 
---
+++ codeforces/1465/B.cpp
@@ -0,0 +1,59 @@
+//Author: AnandRaj uux
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef pair<ll,ll> pll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<pll> vpll; 
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define test() int t;cin>>t;while(t--)
+#define all(v) v.begin(),v.end()
+#define prinp(p) cout<<p.first<<" "<<p.second<<endl
+#define prinv(V) for(auto v:V) cout<<v<<" ";cout<<endl
+#define take(V,f,n) for(int in=f;in<f+n;in++) cin>>V[in]
+#define what(x) cerr<<#x<<" = "<<x<<endl
+#define KStest() int t,t1;cin>>t;t1=t;while(t--)
+#define KScout cout<<"Case #"<<t1-t<<": "
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const int MOD = 1e9+7,MAX = 1e6+5;
+const ll INF = 1e18+5;
+
+bool good(ll n)
+{
+	ll m = n;
+	set<int> D;
+	while(m)
+	{
+		if(m%10==0) m/=10;
+		else
+		{
+			int d = m%10;
+			if(n%d) return false;
+			m/=10;
+		}
+	}
+	return true;
+
+}
+
+int main()
+{
+	test()
+	{
+		ll n;
+		cin>>n;
+		while(1)
+		{
+			if(good(n)) {cout<<n<<endl;break;}
+			else n++;
+		}
+	}
+}	
